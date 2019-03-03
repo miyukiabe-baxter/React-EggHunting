4b6674e6b994d250910f279e4844f2cb742e4c7d
Miyuki Abe <miyuki0809@gmail.com>
2019-03-02 19:37:43 -0500

    right before installing react-google-map

--- client/components/mainPage.js
+++ client/components/mainPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Map from './map'
 
 const MainPage = () => (
   <div>
     <h1>START GAME</h1>
     <h2>Play Game</h2>
+    <Map />
   </div>
 )
 
---
+++ client/components/map.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+class Map extends Component {
+  render() {
+    return <div>Map Template</div>
+  }
+}
+export default Map
