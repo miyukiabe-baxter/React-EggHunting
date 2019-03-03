5fbdb011b7b201aa8bdb24e6552e726c9c76ee62
Miyuki Abe <miyuki0809@gmail.com>
2019-03-03 12:49:26 -0500

    onClick is working but not sure how to change state of marker

--- client/components/map.js
+++ client/components/map.js
@@ -4,10 +4,47 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 
 export class MapContainer extends Component {
   render() {
+    console.log('what is this??', this.props.google)
+    const {Animation} = window.google.maps
+    console.log('checking ', Animation)
+
+    const onMarkerClick = (props, marker, event) => {
+      // props.Animation = {1}
+      // props.visible = false
+      console.log('title', props.title)
+      console.log('animation', props.Animation)
+      console.log('google.maps', props.google.maps.Animation.BOUNCE)
+      console.log('props', props)
+
+      console.log('marker', marker)
+    }
+
     return (
       <div>
-        <Map google={this.props.google} zoom={14}>
-          <Marker onClick={this.onMarkerClick} name="Current location" />
+        <Map
+          google={this.props.google}
+          style={{
+            width: '40%',
+            height: '70%',
+            margin: 'auto',
+            position: 'relative'
+          }}
+          zoom={10}
+        >
+          <Marker
+            title="The marker`s title will appear as a tooltip."
+            name="SOMA"
+            visible={true}
+            onClick={onMarkerClick}
+            Animation={0}
+            position={{lat: 37.778519, lng: -122.40564}}
+          >
+            <Marker onClick={this.onMarkerClick} name="Current location" />
+          </Marker>
+          <Marker
+            name="Dolores park"
+            position={{lat: 37.759703, lng: -122.428093}}
+          />
 
           <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
@@ -17,14 +54,11 @@ export class MapContainer extends Component {
         </Map>
       </div>
     )
-    // return (
-    //   <div>hi hi hi </div>
-    // )
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: GOOGLE_APIKEY
+  apiKey: 'AIzaSyDU4lLRMSBMlKtCbV0PoOEDF6TaNIac6Ck'
 })(MapContainer)
 
 // export default MapContainer
