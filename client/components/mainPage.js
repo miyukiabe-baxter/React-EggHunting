b20a30289f831b0226e55bafc886bf47d81366cb
Miyuki Abe <miyuki0809@gmail.com>
2019-03-02 20:21:36 -0500

    google map is shown. No custamization yet

--- client/components/mainPage.js
+++ client/components/mainPage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Map from './map'
+import MapContainer from './map'
 
 const MainPage = () => (
   <div>
     <h1>START GAME</h1>
     <h2>Play Game</h2>
-    <Map />
+    <MapContainer />
   </div>
 )
 
--- client/components/map.js
+++ client/components/map.js
@@ -1,8 +1,30 @@
 import React, {Component} from 'react'
 
-class Map extends Component {
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
+
+export class MapContainer extends Component {
   render() {
-    return <div>Map Template</div>
+    return (
+      <div>
+        <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick} name="Current location" />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>test</h1>
+            </div>
+          </InfoWindow>
+        </Map>
+      </div>
+    )
+    // return (
+    //   <div>hi hi hi </div>
+    // )
   }
 }
-export default Map
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDU4lLRMSBMlKtCbV0PoOEDF6TaNIac6Ck'
+})(MapContainer)
+
+// export default MapContainer
