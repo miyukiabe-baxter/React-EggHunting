80389a63dee7b885388db54416c56ecfe3222730
Miyuki Abe <miyuki0809@gmail.com>
2019-03-03 20:36:10 -0500

    now dynamically dropping markers

--- client/components/mainPage.js
+++ client/components/mainPage.js
@@ -1,14 +1,60 @@
 import React from 'react'
-
+import axios from 'axios'
 //below is using google-maps-react
 // import MapContainer from './map'
 
 //No API
 
 class MainPage extends React.Component {
-  componentDidMount() {
-    this.renderMap()
+  constructor() {
+    super()
+    this.state = {
+      venues: []
+    }
   }
+
+  componentDidMount() {
+    this.getVenues()
+  }
+
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: 'YTOUTW0MFSVVYABEOEFCPJNPXTMBRY1BMQUHVTRVZMVZT1PN',
+      client_secret: 'SD530C2ZHLQCK44A1SHBJZSYHHXD2NFSDPQNYAULPW2KB2OB',
+      query: 'coffee',
+      near: 'New York',
+      v: '20180323'
+    }
+
+    //this.setState takes callback function. because we need to make sure this.getVenues to happen first.
+    //Then, once this.state.venues are filled with data, let's call this.renderMap()
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState(
+          {
+            venues: response.data.response.groups[0].items
+          },
+          this.renderMap()
+        )
+      })
+      .catch(error => {
+        console.log('error!1 ' + error)
+      })
+  }
+
+  // getTrivia = () => {
+  //   const endPoint = 'https://opentdb.com/api.php?amount=20&category=18'
+
+  //   axios.get(endPoint)
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+  //   .catch(error => {
+  //     console.log("error!1 " + error)
+  //   })
+  // }
 
   renderMap = () => {
     loadScript(
@@ -22,9 +68,20 @@ class MainPage extends React.Component {
       center: {lat: 40.748287, lng: -73.989794},
       zoom: 12
     })
+    this.state.venues.map(myVenue => {
+      var marker = new window.google.maps.Marker({
+        position: {
+          lat: myVenue.venue.location.lat,
+          lng: myVenue.venue.location.lng
+        },
+        map: map,
+        title: myVenue.venue.name
+      })
+    })
   }
 
   render() {
+    console.log(this.state.venues)
     return (
       <div>
         <div>
@@ -43,11 +100,6 @@ class MainPage extends React.Component {
       </div>
     )
   }
-}
-
-{
-  /* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-    async defer></script> */
 }
 
 function loadScript(url) {
